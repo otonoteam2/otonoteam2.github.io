0b4a291c187eb2c679a4c644c80439f41b5e8e35
otonoteam2 <79968699+otonoteam2@users.noreply.github.com>
2021-11-16 17:04:49 +0800

    TERRANCE

--- task024.js
+++ task024.js
@@ -1,3 +1,8 @@
+//Define vars to hold time values
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
 //Define vars to hold "display" value
 let displaySeconds = 0;
 let displayMinutes = 0;
@@ -12,44 +17,44 @@ let status = "stopped";
 //Stopwatch function (logic to determine when to increment next value, etc.)
 function stopWatch(){
 
-seconds++;
+    seconds++;
 
-//Logic to determine when to increment next value
-if(seconds / 60 === 1){
-seconds = 0;
-minutes++;
+    //Logic to determine when to increment next value
+    if(seconds / 60 === 1){
+        seconds = 0;
+        minutes++;
 
-if(minutes / 60 === 1){
-minutes = 0;
-hours++;
-}
+        if(minutes / 60 === 1){
+            minutes = 0;
+            hours++;
+        }
 
-}
+    }
 
-//If seconds/minutes/hours are only one digit, add a leading 0 to the value
-if(seconds < 10){
-displaySeconds = "0" + seconds.toString();
-}
-else{
-displaySeconds = seconds;
-}
+    //If seconds/minutes/hours are only one digit, add a leading 0 to the value
+    if(seconds < 10){
+        displaySeconds = "0" + seconds.toString();
+    }
+    else{
+        displaySeconds = seconds;
+    }
 
-if(minutes < 10){
-displayMinutes = "0" + minutes.toString();
-}
-else{
-displayMinutes = minutes;
-}
+    if(minutes < 10){
+        displayMinutes = "0" + minutes.toString();
+    }
+    else{
+        displayMinutes = minutes;
+    }
 
-if(hours < 10){
-displayHours = "0" + hours.toString();
-}
-else{
-displayHours = hours;
-}
+    if(hours < 10){
+        displayHours = "0" + hours.toString();
+    }
+    else{
+        displayHours = hours;
+    }
 
-//Display updated time values to user
-document.getElementById("display").innerHTML =Math.floor(Math.random()*100);
+    //Display updated time values to user
+    document.getElementById("display").innerHTML =Math.floor(Math.random()*100);
 }
 
 
@@ -57,20 +62,20 @@ document.getElementById("display").innerHTML =Math.floor(Math.random()*100);
 
 function startStop(){
 
-if(status === "stopped"){
+    if(status === "stopped"){
 
-//Start the stopwatch (by calling the setInterval() function)
-interval = window.setInterval(stopWatch, 0);
-document.getElementById("startStop").innerHTML = "停止抽獎";
-status = "started";
+        //Start the stopwatch (by calling the setInterval() function)
+        interval = window.setInterval(stopWatch, 0);
+        document.getElementById("startStop").innerHTML = "ラッキードローをやめる";
+        status = "started";
 
-}
-else{
+    }
+    else{
 
-window.clearInterval(interval);
-document.getElementById("startStop").innerHTML = "重新抽獎";
-status = "stopped";
+        window.clearInterval(interval);
+        document.getElementById("startStop").innerHTML = "再幸運なクロウ";
+        status = "stopped";
 
-}
+    }
 
 }
